acd5ff252ea3d56ed07eae5dbecb4647a572a583
jamilly <jamilly.oliveira2604@gmail.com>
2023-02-28 13:51:32 -0300

    segundo commit

--- app/views.py
+++ app/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
 
+# Create your views here.
 def home(request):
     #return HttpResponse("<h1> Olá mundo Ufra!</h1>")
     return render(request,'index.html')
 
 
+def login(request):
+    return render(request, 'login.html')
+
+def create_doc(request):
+    return render(request, 'create_doc.html')
+
+def read_doc(request):
+    return render(request, 'read_doc.html')
--- project/wsgi.py
+++ project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for project project.
+WSGI config for project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -12,5 +12,3 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
-
-application = get_wsgi_application()
